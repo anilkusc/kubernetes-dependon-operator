3c5e9b6c60bada9da9cbfe06cf8fe45516759a67
C0NO <anil@anil.com>
2020-05-03 18:36:08 +0300

    test

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 
 	//fmt.Println(MakeReqGet("/api/v1/namespaces/default/pods/"))
-	fmt.Println(MakeReqStream("/api/watch/v1/namespaces/default/pods/"))
+	MakeReqStream("/api/watch/v1/namespaces/default/pods/")
 
 }
