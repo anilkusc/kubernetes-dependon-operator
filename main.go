b06cbf8654e77bea28064f529870475e89a651da
C0NO <anil@anil.com>
2020-05-07 21:45:15 +0300

    test

--- main.go
+++ main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
-	"os"
+	"fmt"
+	"io/ioutil"
 )
 
+/*
 const (
 	address string = "https://kubernetes"
 )
-
+*/
 func main() {
 
+	dat, _ := ioutil.ReadFile("/run/secrets/kubernetes.io/serviceaccount/namespace")
+	fmt.Print(string(dat))
 	//fmt.Println(MakeReqGet("/api/v1/namespaces/default/pods/"))
 
-	MakeReqStream("/api/v1/watch/namespaces/default/pods/management-748d988bf8-h8s6m")
+	//MakeReqStream("/api/v1/watch/namespaces/default/pods/management-748d988bf8-h8s6m")
 
-}
-
-func get_namespace() string {
-	//TODO: Get namespace automatically
-	return os.Getenv("NAMESPACE")
 }
