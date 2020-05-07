bcae2b0d80939bd5bdcb6f1c189305ccfd8a2a2e
C0NO <anil@anil.com>
2020-05-07 21:47:48 +0300

    test

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ const (
 func main() {
 
 	namespace, _ := ioutil.ReadFile("/run/secrets/kubernetes.io/serviceaccount/namespace")
-	fmt.Println("Namespace is:", namespace)
+	fmt.Print("Namespace is :", string(namespace))
 	//fmt.Println(MakeReqGet("/api/v1/namespaces/default/pods/"))
 
 	//MakeReqStream("/api/v1/watch/namespaces/default/pods/management-748d988bf8-h8s6m")
