9ecf750c5f91f25c9867fe6b8d2e5bf04216bb24
C0NO <anil@anil.com>
2020-05-09 18:18:05 +0300

    updated

--- dependon.go
+++ dependon.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/tidwall/gjson"
 )
@@ -10,14 +11,16 @@ import (
 const (
 	deployment_url  string = "https://kubernetes/apis/apps/v1/namespaces/" + os.Getenv("NAMEPSACE") + "/deployments/"
 	statefulset_url string = "https://kubernetes/apis/apps/v1/namespaces/" + os.Getenv("NAMEPSACE") + "/statefulsets/"
+	dependons_url   string = "https://kubernetes/apis/anilkuscu.github.com/v1alpha1/namespaces/" + os.Getenv("NAMEPSACE") + "/dependons/"
 )
 
 func Trigger(json string) {
 
 	workload_type := gjson.Get(json, "object.kind").String()
+
 	if workload_type == "Deployment" {
 
-	} else if workload_type == "Statefulset" {
+	} else if workload_type == "StatefulSet" {
 
 	}
 
@@ -49,4 +52,24 @@ func Start_statefulset(statefulset_name string) {
 	data := `{"spec":{"replicas":1}}`
 	MakeReqPatch(url, data)
 	fmt.Println("Statefulset " + statefulset_name + "has been started.")
+}
+
+func Get_dependents() ([]string, []string) {
+
+}
+
+func Get_centrals() {
+
+}
+
+func Get_dependons() []string {
+	var dependon_names []string
+	dependons_info := MakeReqGet(dependons_url)
+	dependons := gjson.Get(dependons_info, "items.#")
+	dependons_count := int(dependons.Int())
+	for i := 0; i < dependons_count; i++ {
+		count := strconv.Itoa(i)
+		dependon_names[i] = gjson.Get(dependons_info, "items."+count+".metadata.name").String()
+	}
+	return dependon_names
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 )
@@ -13,7 +14,7 @@ const (
 func main() {
 	namespace, _ := ioutil.ReadFile("/run/secrets/kubernetes.io/serviceaccount/namespace")
 	os.Setenv("NAMESPACE", string(namespace))
-
+	fmt.Println(Get_dependons())
 	//fmt.Println(MakeReqGet("/api/v1/namespaces/default/pods/"))
 
 	//MakeReqStream("/api/v1/watch/namespaces/default/pods/management-748d988bf8-h8s6m")
