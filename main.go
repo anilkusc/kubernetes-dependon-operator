3799b1ac88555cfc82d7b9d8e7e75667ac7489ff
C0NO <anil@anil.com>
2020-05-16 15:51:19 +0300

    added waitgroup

--- main.go
+++ main.go
@@ -3,15 +3,11 @@ package main
 import (
 	"io/ioutil"
 	"os"
+	"sync"
 )
 
 var restart_all bool = false
 
-/*
-const (
-	address string = "https://kubernetes"
-)
-*/
 func main() {
 	namespace, _ := ioutil.ReadFile("/run/secrets/kubernetes.io/serviceaccount/namespace")
 	os.Setenv("NAMESPACE", string(namespace))
@@ -20,8 +16,13 @@ func main() {
 
 		restart_all = false
 		for restart_all == false {
-			go Dependons_controller()
-			select {}
+			var wg sync.WaitGroup
+			wg.Add(1)
+			go func() {
+				Dependons_controller()
+				wg.Done()
+			}()
+			wg.Wait()
 		}
 
 	}
