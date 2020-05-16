bbe48ce26a77ff4be03cd0e7251bc92e7cfab957
C0NO <anil@anil.com>
2020-05-16 12:54:54 +0300

    test

--- logic.go
+++ logic.go
@@ -12,20 +12,15 @@ func Dependons_controller() {
 
 	for {
 		new_dependons = Get_dependons()
-		fmt.Println("1")
 		for i, _ := range dependons {
-			fmt.Println("2")
 			if new_dependons[i] == dependons[i] {
-				fmt.Println("3")
 				continue
-
 			} else {
 				fmt.Println("Dependons has been changed.")
 				break
 			}
 
 		}
-		fmt.Println("4")
 
 	}
 	fmt.Println("Restarted all")
